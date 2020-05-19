4e35cf690a9c58ad89fe6bcb41951c939fed0419
Erdinc <Erdinc@gklfmg.com>
2020-05-19 11:24:20 +0200

    corrections based on feedbacks

--- Week1/mysqlexercises/exercise1.js
+++ Week1/mysqlexercises/exercise1.js
@@ -11,15 +11,24 @@ const connection = mysql.createConnection({
 const execQuery = util.promisify(connection.query.bind(connection));
 
 const seedDatabase = async () => {
-  const createInvitee =
-    "CREATE TABLE IF NOT EXISTS Invitee (invitee_no int(3) ZEROFILL NOT NULL, invitee_name varchar(20), invited_by varchar(20), CONSTRAINT PK_Invitee_No PRIMARY KEY (invitee_no))";
-  const createRoom =
-    "CREATE TABLE IF NOT EXISTS Room ( room_no int NOT NULL, room_name varchar(20), floor_number int, CONSTRAINT PK_Room_No PRIMARY KEY (room_no))";
-  const createMeeting =
-    "CREATE TABLE IF NOT EXISTS Meeting ( meeting_no int NOT NULL AUTO_INCREMENT, meeting_title varchar(20), starting_time DATETIME NOT NULL , ending_time DATETIME, room_no int NOT NULL, CONSTRAINT PK_Meeting_No PRIMARY KEY (meeting_no))";
+  const createInvitee = `CREATE TABLE IF NOT EXISTS Invitee (invitee_no int(3)
+                       ZEROFILL NOT NULL,
+                       invitee_name varchar(20), 
+                       invited_by varchar(20), 
+                       CONSTRAINT PK_Invitee_No PRIMARY KEY (invitee_no))`;
+  const createRoom = `CREATE TABLE IF NOT EXISTS Room 
+                    ( room_no int NOT NULL, room_name varchar(20), 
+                    floor_number int, 
+                    CONSTRAINT PK_Room_No PRIMARY KEY (room_no))`;
+  const createMeeting = `CREATE TABLE IF NOT EXISTS Meeting 
+                      ( meeting_no int NOT NULL AUTO_INCREMENT, meeting_title varchar(20), starting_time DATETIME NOT NULL , ending_time DATETIME, room_no int NOT NULL, CONSTRAINT PK_Meeting_No PRIMARY KEY (meeting_no))`;
 
   connection.connect();
   try {
+    await executeQuery("DROP DATABASE IF EXISTS meetup");
+    await executeQuery("CREATE DATABASE IF NOT EXISTS meetup");
+    await executeQuery("USE meetup");
+
     await Promise.all[
       (execQuery(createInvitee), execQuery(createRoom), execQuery(createMeeting))
     ];
@@ -38,4 +47,4 @@ const seedDatabase = async () => {
   }
 };
 
-seedDatabase()
+seedDatabase();
--- Week1/mysqlexercises/exercise1Tables.js
+++ Week1/mysqlexercises/exercise1Tables.js
@@ -2,12 +2,16 @@ const tables = {
   Invitee: [
     { invitee_no: 1, invitee_name: "Aqwd", invited_by: "Bwer" },
     { invitee_no: 2, invitee_name: "Bwer", invited_by: "Cwer" },
-    { invitee_no: 3, invitee_name: "Cwer", invited_by: "Dwer" },
+    { invitee_no: 3, invitee_name: "Cwr", invited_by: "Der" },
+    { invitee_no: 4, invitee_name: "Cer", invited_by: "wer" },
+    { invitee_no: 5, invitee_name: "wer", invited_by: "Dwe" },
   ],
   Room: [
     { room_no: 100, room_name: "100A", floor_number: 1 },
     { room_no: 201, room_name: "201B", floor_number: 2 },
     { room_no: 302, room_name: "302C", floor_number: 3 },
+    { room_no: 402, room_name: "402D", floor_number: 4 },
+    { room_no: 502, room_name: "502E", floor_number: 5 },
   ],
   Meeting: [
     {
@@ -25,8 +29,20 @@ const tables = {
     {
       meeting_title: "Computer",
       starting_time: "2222-04-03 09:00:00",
-      ending_time: "2222-04-03 10:00:00",
+      ending_time: "2226-04-03 10:00:00",
       room_no: 3,
+    },
+    {
+      meeting_title: "Job",
+      starting_time: "2320-05-01 09:00:00",
+      ending_time: "2324-06-01 10:00:00",
+      room_no: 4,
+    },
+    {
+      meeting_title: "Job",
+      starting_time: "2320-05-01 09:00:00",
+      ending_time: "2321-06-01 10:00:00",
+      room_no: 5,
     },
   ],
 };
